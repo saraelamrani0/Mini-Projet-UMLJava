ad0bd7bec1f22947e2e488622855057c6794d153
MuhammadModibbo <72864219+MuhammadModibbo@users.noreply.github.com>
2025-11-23 11:57:00 +0100

    Create AdherentDAO.java

---
+++ src/main/java/ma/est/biblio/dao/AdherentDAO.java
@@ -0,0 +1,13 @@
+package ma.est.biblio.dao;
+
+public class AdherentDAO {
+	public static void main(String[] args) {
+	public void save (Adherent adherent) {}
+	public Adherent findByID(int id) {
+		return null;
+		}
+		public void delete(int id){}
+
+	}
+
+}
