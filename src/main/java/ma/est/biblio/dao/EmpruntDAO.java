66d69b4ccd3fa1679290d3d4c90af9f6c6e3bffc
MuhammadModibbo <72864219+MuhammadModibbo@users.noreply.github.com>
2025-11-23 11:57:24 +0100

    Create EmpruntDAO.java

---
+++ src/main/java/ma/est/biblio/dao/EmpruntDAO.java
@@ -0,0 +1,12 @@
+package ma.est.biblio.dao;
+
+public class EmpruntDAO {
+		public void save (Emprunt emprunt) {}
+		public Emprunt findByID(int id) {
+			return null;
+		}
+		public void delete(int id){}
+
+	}
+
+}
