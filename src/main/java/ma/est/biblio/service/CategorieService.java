ee46da634fd2a9d4bfd92598c266af35737dcdf5
MuhammadModibbo <72864219+MuhammadModibbo@users.noreply.github.com>
2025-11-23 20:13:30 +0100

    Create CategorieService.java

---
+++ src/main/java/ma/est/biblio/service/CategorieService.java
@@ -0,0 +1,9 @@
+package ma.est.biblio.service;
+
+public class CategorieService {
+
+	public static void ajouterCategorie(CategorieService categorie) {}
+	
+	public void supprimerLivre(int id) {}
+
+}
