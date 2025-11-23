7c001eef530d99a44e63a762d39917e6a786932f
MuhammadModibbo <72864219+MuhammadModibbo@users.noreply.github.com>
2025-11-23 20:13:36 +0100

    Create PersonneDAO.java

---
+++ src/main/java/ma/est/biblio/dao/PersonneDAO.java
@@ -0,0 +1,14 @@
+package ma.est.biblio.dao;
+
+public class PersonneDAO {
+	public static void main(String[] args) {
+	
+	public void save(Personne p)	{}
+
+	public Utilisateur findByUsername(String username) {
+		return null;
+		}
+
+	}
+
+}
