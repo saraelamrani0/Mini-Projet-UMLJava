a6896f81a890982543fe970d707c58237b1c1ae9
MuhammadModibbo <72864219+MuhammadModibbo@users.noreply.github.com>
2025-11-23 20:13:42 +0100

    Create Categorie.java

---
+++ src/main/java/ma/est/biblio/dao/Categorie.java
@@ -0,0 +1,14 @@
+package ma.est.biblio.dao;
+
+public class CatagorieDAO {
+	public static void main(String[] args) {
+	
+	public void save(Categorie categorie)	{}
+
+	public Categorie findById(int id) {
+		return null;
+		}
+
+	}
+public void delete(int id) {}
+}
