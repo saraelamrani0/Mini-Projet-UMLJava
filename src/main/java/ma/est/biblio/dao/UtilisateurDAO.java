045305265c445d71bd15aa8d9d22b263551bba99
MuhammadModibbo <72864219+MuhammadModibbo@users.noreply.github.com>
2025-11-23 11:57:34 +0100

    Create UtilisateurDAO.java

---
+++ src/main/java/ma/est/biblio/dao/UtilisateurDAO.java
@@ -0,0 +1,12 @@
+package ma.est.biblio.dao;
+
+public class UtilisateurDAO {
+	public static void main(String[] args) {
+	public void save(Utilisateur u)	{}
+	public Utilisateur findByUsername(String username) {
+		return null;
+		}
+
+	}
+
+}
